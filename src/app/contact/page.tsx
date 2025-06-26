5849c8da859b8ebd0c752c7579a4b3371b6ef7ef
Aryan Delawadia <aryandelwadia@gmail.com>
2025-06-26 15:39:48 +0530

    commit

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,63 @@
 "use client"
 import { motion } from "motion/react";
+import { useState } from "react";
+import { useRef } from "react";
+import emailjs from '@emailjs/browser';
 
 export default function ContactPage(){
+    
+    const [success, setSuccess] = useState(false);
+    const [error, setError] = useState(false);
+
+    const text = "Say Hello"
+
+    const form = useRef<HTMLFormElement | null>(null);
+
+    const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setError(false);
+        setSuccess(false);
+
+        emailjs
+        .sendForm(process.env.NEXT_PUBLIC_SERVICE_ID!, process.env.NEXT_PUBLIC_TEMPLATE_ID!, form.current!, {
+            publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY!,
+        })
+        .then(
+            () => {
+                setSuccess(true);
+                form.current?.reset();
+            },
+            (error) => {
+                setError(true);
+                console.log(error);
+            },
+        );
+    };
+    
     return (
         <motion.div className="h-full" initial={{y:"-200vh"}} animate={{y:"0%"}} transition={{duration: 1}}>
-            contact
+            <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+                <div className="h-1/2 lg:h-full lg:w-1/2 flex items-center justify-center text-6xl">
+                    <motion.div>
+                        {text.split("").map((letter, index) =>(
+                            <motion.span key={index} initial={{opacity: 1}} animate={{opacity: 0}} transition={{duration: 3, repeat: Infinity, delay: index*0.1}}>
+                                {letter}
+                            </motion.span>
+                        ))}
+                    </motion.div>
+                </div>
+
+                <form ref={form} onSubmit={sendEmail} className="h-1/2 lg:h-full lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-24 " action="">
+                    <span>Dear Aryan Delawadia</span>
+                    <textarea name="user_message" id="" rows={6} className="bg-transparent border-b-2 border-b-black outline-none resize-none"></textarea>
+                    <span>My mail address is:</span>
+                    <input name="user_email" type="text" className="bg-transparent border-b-2 border-b-black outline-none resize-none"/>
+                    <span>Regards</span>
+                    <button type="submit" className="bg-purple-200 rounded font-semibold text-gray-600 p-4 ">Send</button>
+                    {success && <span className="text-green-600 font-semibold">Your message has been sent Successfully!</span>}
+                    {error && <span className="text-red-600 font-semibold">Something went Wrong!</span>}
+                </form>
+            </div>
         </motion.div>
     );
 }
--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,10 +1,61 @@
 "use client"
-import { motion } from "motion/react";
+import { motion, useScroll, useTransform } from "motion/react";
+import { useRef } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+const items = [
+    {
+        id: 1,
+        color: "from-red-300 to-blue-300",
+        title: "Fresh Eats",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro optio sed, labore fuga quam doloribus qui unde iusto! Praesentium aliquam ratione vitae saepe consectetur. Laudantium qui velit necessitatibus fuga id.",
+        img: "public\next.svg",
+        link: "",
+    },
+    {
+        id: 2,
+        color: "from-blue-300 to-violet-300",
+        title: "Fresh Eats",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro optio sed, labore fuga quam doloribus qui unde iusto! Praesentium aliquam ratione vitae saepe consectetur. Laudantium qui velit necessitatibus fuga id.",
+        img: "public\next.svg",
+        link: "",
+    },
+];
 
 export default function PortfolioPage(){
+    
+    const ref = useRef<HTMLDivElement | null>(null);
+    const {scrollYProgress} = useScroll({target: ref});
+
+    const x = useTransform(scrollYProgress, [0, 1], ["0%", "-66%"])
+    
     return (
         <motion.div className="h-full" initial={{y:"-200vh"}} animate={{y:"0%"}} transition={{duration: 1}}>
-            Portfolio Page
+            <div className="h-[400vh] relative" ref={ref}>
+                <div className="w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-8xl text-center">
+                    My Works
+                </div>
+                <div className="sticky top-0 flex h-screen gap-4 items-center overflow-hidden">
+                    <motion.div style={{ x }} className="flex">
+                        <div className="h-screen w-screen flex items-center justify-center bg-gradient-to-r from-purple-300 to-red-300" />
+                        {items.map(item => (
+                            <div className={`h-screen w-screen flex items-center justify-center bg-gradient-to-r ${item.color}`} key={item.id} >
+                                <div className=" flex flex-col gap-8 text-white">
+                                    <h1 className="text-xl font-bold md:text-4xl lg:text-6xl xl:text-8xl">{item.title}</h1>
+                                    <div className="relative w-80 h-56 md:w-96 md:h-64 lg:w-[500px] lg:h-[350px] xl:w-[600px] xl:h-[420px]">
+                                        <Image src={item.img} alt="" fill />
+                                    </div>
+                                    <p className="w-80 md:w-96 lg:w-[500px] xl:w-[600px] lg:text-xl">{item.desc}</p>
+                                    <Link href={item.link} className="flex justify-end">
+                                        <button className="p-2 text-sm md:p-4 md:text-md lg:p-8 lg:text-lg bg-white text-gray-600 font-semibold m-4 rounded">Github</button>
+                                    </Link>
+                                </div>
+                            </div>
+                        ))}
+                    </motion.div>
+                </div>
+            </div>
         </motion.div>
     );
 }
