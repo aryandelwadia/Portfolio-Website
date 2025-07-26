4b119d490d6fd920327ab5b816e0018275a3245d
Aryan Delawadia <aryandelwadia@gmail.com>
2025-07-26 16:47:47 +0530

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
           <h1 className="text-4xl font-bold md:text-6xl">Turning Ideas Into Functional Code</h1>
-          <p className="md:text-xl">Hello! I'm a curious and motivated software developer, just starting out but full of ideas and energy. I love turning concepts into code and learning new things every day. From small scripts to full-stack projects, I'm always excited to build, collaborate, and grow. Thanks for stopping by!</p>
+          <p className="md:text-xl">{"Hello! I'm a curious and motivated software developer, just starting out but full of ideas and energy. I love turning concepts into code and learning new things every day. From small scripts to full-stack projects, I'm always excited to build, collaborate, and grow. Thanks for stopping by!"}</p>
           <div className="flex gap-4 ">
             <Link href={"/contact"}>
               <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">Contact Me</button>
